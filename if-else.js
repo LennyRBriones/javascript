d3ed56d42313efc4b001d7cbac9d669e4aaeef21
Lenny R. Briones <98720548+LennyRBriones@users.noreply.github.com>
2023-08-15 14:10:12 -0600

    adding codes from windows

---
+++ if-else.js
@@ -0,0 +1,11 @@
+var age = 56
+
+if (age === 18) {
+    console.log("Welcome to your first vote");
+
+} else if (age > 18) {
+    console.log("Welcome to vote again");
+
+} else if (age < 18) {
+    console.log("Sorry, you´re no able to vote");
+}
---
+++ ternary-conditional-operator.js
@@ -0,0 +1,10 @@
+var number = 1;
+
+var result = number === 1 ? "Yes I´m the number 1" : "Sorry, I´m not the numner 1";
+console.log(result)
+
+
+var number = 3;
+
+var result = number === 1 ? "yes I´m the number 1" : "Sorry, I´m not the numner 1";
+console.log(result)
